d6d979d38cc3b03a71a4aec406304c385e18bd8b
Rawan Saab <rawansaab777@gmail.com>
2026-08-27 19:11:04 +0300

    docs: add client script documentation

---
+++ public/profile.js
@@ -0,0 +1,9 @@
+/**
+* Names: Rawan Saab (213693625), Lareen Kadour (213992431), George Hanna (324090968)
+* Date: July / August 2026
+* Github URL: https://github.com/rawansaab/ex1b
+* Description:
+* Client-side code for the dynamic animal profile page.
+* The script reads the profile id from the URL and will use Fetch
+* to receive profile information from the server.
+*/
